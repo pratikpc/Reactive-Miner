4b1fc2530bfac1fd7ff44de7a62fe1154bc67111
Pratik Chowdhury <pratikc@live.co.uk>
2020-04-10 21:31:41 +0530

    Added Common Machine Learning Utils

--- src/components/ml/utils.js
+++ src/components/ml/utils.js
@@ -1,5 +1,21 @@
 import * as tf from "@tensorflow/tfjs";
 
+export function Normalize(tensor) {
+    const max = tensor.max();
+    const min = tensor.min();
+    const normalized = NormalizeVal(max, min, tensor);
+    return { max, min, normalized };
+}
+
+export function NormalizeVal(max, min, value){
+    const normalized = value.sub(min).div(max.sub(min));
+    return normalized;
+}
+
+export function InputDimSize(tensor) {
+    return tensor.shape[1];
+}
+
 export function ReadCSV(url, delimiter){
     const data = tf.data.csv(url, {
         hasHeader: true,
