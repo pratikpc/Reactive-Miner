680a5225dc790e785df1f9713d59037cb8739ec9
Pratik Chowdhury <pratikc@live.co.uk>
2020-04-11 07:04:43 +0530

    Added Common Tensor Components

--- src/components/ml/utils.js
+++ src/components/ml/utils.js
@@ -1,9 +1,42 @@
-import * as tf from "@tensorflow/tfjs";
+import * as tfdata from "@tensorflow/tfjs-data";
+import * as tfcore from "@tensorflow/tfjs-core";
+import * as tfvis from "@tensorflow/tfjs-vis";
 
-export function ReadCSV(url, delimiter){
-    const data = tf.data.csv(url, {
+export function ReadCSV(url, delimiter) {
+    const data = tfdata.csv(url, {
         hasHeader: true,
         delimiter: delimiter
     });
     return data;
+}
+export function SetDifference(a, b) {
+    return a.filter(x => !b.includes(x));
+}
+
+export function CreateTensor(data, labels) {
+    const arr = [];
+    for (const label of labels)
+        arr.push(data[label]);
+    return tfcore.tensor(arr);
+}
+
+export function Zip(a, b) {
+    const c = a.map(function (e, i) {
+        return [e, b[i]];
+    });
+    return c;
+}
+export async function DrawChart(x, y, preds) {
+    const series_names = [];
+    const series = [];
+    for (let i = 0; i < x[0].length; ++i) {
+        series_names.push("X " + i + " Y");
+        series.push(Zip(x, y).map(([x, y]) => { return { x: x[i], y }; }));
+        series_names.push("X " + i + " PRED");
+        series.push(Zip(x, preds).map(([x, y]) => { return { x: x[i], y }; }));
+    }
+    const data = { values: series, series: series_names }
+
+    const surface = { name: 'Line chart', tab: 'Plot' };
+    await tfvis.render.scatterplot(surface, data);
 }
