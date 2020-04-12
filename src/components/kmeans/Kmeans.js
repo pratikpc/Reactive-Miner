1e0fee1dc5c7647a3a5db99783f769037edf544e
Pratik Chowdhury <pratikc@live.co.uk>
2020-04-12 15:07:50 +0530

    Update Kmeans.js

--- src/components/kmeans/Kmeans.js
+++ src/components/kmeans/Kmeans.js
@@ -1,18 +1,39 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './kmeans.css';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';;
+import Grid from '@material-ui/core/Grid';
+import { csvContext } from '../context/csv-context';
+import { ExtractSelectedLabelsFromCSV } from '../../ML/utils';
+
+async function kmeansFunction(csv, selectedCols) {
+    const data = await ExtractSelectedLabelsFromCSV(csv, selectedCols);
+    console.log(selectedCols);
+    console.log('Function called', data);
+}
 
 const Kmeans = () => {
 
-    const kmeansFunction = () => {
-        console.log('Function called');
-    }
+    const { csv } = useContext(csvContext);
+    const [columnNames, setColumnNames] = useState([]);
+    useEffect(() => {
+        if (csv == null)
+            return;
+        async function LoadColumnNames() {
+            const columns = await csv.columnNames();
+            setColumnNames(columns);
+            setYcolumn(columns[columns.length - 1]);
+        }
+        LoadColumnNames();
+    }, [csv, setColumnNames]);
 
     return (
         <Grid container>
             <Grid item md={6} xs={12}>
-                <Button onClick={kmeansFunction}>Click</Button>
+                <CsvReader />
+                <CsvTable />
+            </Grid>
+            <Grid item md={6} xs={12}>
+                <Button onClick={await kmeansFunction(csv, columnNames)}>Click</Button>
             </Grid>
         </Grid>
     );
