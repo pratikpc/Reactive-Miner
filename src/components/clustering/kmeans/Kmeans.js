581e2a590abc43b4b86d0e9b860fae6760d4cf69
Rahim Chunara <rahimchunara55@mgial.com>
2020-04-13 12:24:22 +0530

    Added KMeans Visualization

--- src/components/clustering/kmeans/Kmeans.js
+++ src/components/clustering/kmeans/Kmeans.js
@@ -5,22 +5,71 @@ import './kmeans.css';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { csvContext } from '../../context/csv-context';
-import { ExtractSelectedLabelsFromCSV, ConvertCSVToRawArrays } from '../../../ML/utils';
-import kmeans from 'ml-kmeans';
+import { ConvertCSVToRawArrays } from '../../../ML/utils';
+import Plot from 'react-plotly.js';
+import {KMeans} from 'shaman';
 
-async function kmeansFunction(csv, selectedCols, k=3) {
+async function kmeansFunction(csv, selectedCols, plotData, setplotData, k = 3) {
     const [data, _] = await ConvertCSVToRawArrays(csv, [], selectedCols);
     console.log(selectedCols);
     console.log('Function called', data);
-    // Initialize the magicFeature
-    const result = kmeans(data, k);
 
-    console.log(result);
+    const kmeans = new KMeans(k);
+
+    // execute clustering using dataset
+    kmeans.cluster(data, function (err, clusters, centroids) {
+        // show any errors
+        console.log(err);
+
+        // dictionary for aux the indexes read
+        const indexes = {
+            X: 0,
+            Y: 1,
+        }
+
+        // build centroids graph model
+        const centroidTrace = {
+            x: centroids.map(function (c) {
+                return c[indexes["X"]]; 
+            }),
+            y: centroids.map(function (c) {
+                return c[indexes["Y"]]; 
+            }),
+            mode: 'markers',
+            type: 'scatter',
+            name: 'Centroids',
+            marker: {
+                color: '#000000',
+                symbol: 'cross',
+                size: 10
+            }
+        };
+
+        // adding centroids data on the plotData array
+        setplotData(plotData = [centroidTrace]);
+        // build clusters graph model
+        clusters.forEach(function (cluster, index) {
+            const trace = {
+                x: cluster.map(function (c) {
+                    return c[indexes["X"]];
+                }),
+                y: cluster.map(function (c) {
+                    return c[indexes["Y"]];
+                }),
+                jitter: 0.3,
+                mode: 'markers',
+                type: 'scatter',
+                name: 'Cluster ' + index
+            }
+            // add cluster graph data on plotData
+            setplotData(plotData => [...plotData, trace]);
+        });
+    })
 }
-
 
 const Kmeans = () => {
 
+    const [plotData, setplotData] = useState();
     const { csv } = useContext(csvContext);
     const [columnNames, setColumnNames] = useState([]);
     useEffect(() => {
@@ -41,7 +90,8 @@ const Kmeans = () => {
                 <CsvTable />
             </Grid>
             <Grid item md={6} xs={12}>
-                <Button onClick={async () => await kmeansFunction(csv, columnNames)}>Click</Button>
+                <Button onClick={async () => await kmeansFunction(csv, columnNames, plotData, setplotData)}>Click</Button>
+                <Plot {...console.log(plotData)} data={plotData}/>
             </Grid>
         </Grid>
     );
