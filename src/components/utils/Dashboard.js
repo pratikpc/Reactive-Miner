692e71abe2cada8d68f2bfd81f54e46a34cf8070
Princeton Baretto <princebaretto@gmail.com>
2020-04-13 22:48:59 +0530

    added ml5 image with card

--- src/components/utils/Dashboard.js
+++ src/components/utils/Dashboard.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import logo from './logo.png'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
     root: {
       maxWidth: 345,
+    },
+    media: {
+      height: 140,
     },
 });
 
@@ -21,7 +32,50 @@ const Dashboard = () => {
                 </div>
             </Grid>
             <Grid item md={6} xs={12}>
-            
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                        className={classes.media}
+                        image="./tensorflow.png"
+                        title="Tensorflow JS"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            TensorFlow.JS
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            TensorFlow.js is a library for machine learning in JavaScript
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            <Link to="https://www.tensorflow.org/js" > See More </Link> 
+                        </Button>
+                    </CardActions>
+                </Card>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                        className={classes.media}
+                        image="./ml5.png"
+                        title="Ml5.JS"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Ml5.JS
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            The library provides access to machine learning algorithms and models in the browser, building on top of TensorFlow.js with no other external dependencies.
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                            <Link to="https://ml5js.org/" > See More </Link> 
+                        </Button>
+                    </CardActions>
+                </Card>
             </Grid>
         </Grid>
     )
