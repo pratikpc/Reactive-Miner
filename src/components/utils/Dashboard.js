9b7db7a18c8dc975833928b34d7fd7a63474cb8f
amurto <amurtobasu@gmail.com>
2020-04-13 21:43:15 +0530

    db

--- src/components/utils/Dashboard.js
+++ src/components/utils/Dashboard.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import logo from './logo.png'
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+});
 
 const Dashboard = () => {
+    // eslint-disable-next-line
+    const classes = useStyles();
+
     return (
-        <div>
-            fg
-        </div>
+        <Grid container>
+            <Grid item md={6} xs={12}>
+                <div style={{ textAlign: 'center' }}>
+                    <img className="dashboard-img" src={logo} alt="dashboard-log" />
+                </div>
+            </Grid>
+            <Grid item md={6} xs={12}>
+            
+            </Grid>
+        </Grid>
     )
 }
 
